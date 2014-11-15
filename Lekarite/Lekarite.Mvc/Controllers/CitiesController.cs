1ee48423a28e7a6a7246f74816d616e259d972da
Nikolay Dimitrov <niko_dimitroff@yahoo.com>
2014-11-15 13:56:02 +0200

    Implement Cities/All

--- Lekarite/Lekarite.Mvc/Controllers/CitiesController.cs
+++ Lekarite/Lekarite.Mvc/Controllers/CitiesController.cs
@@ -1,16 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Lekarite.Mvc.Controllers
+﻿namespace Lekarite.Mvc.Controllers
 {
-    public class CitiesController : Controller
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using PagedList;
+    using AutoMapper.QueryableExtensions;
+
+    using Lekarite.Data.Interfaces;
+    using Lekarite.Mvc.Models.Cities;
+
+    public class CitiesController : BaseController
     {
-        public ActionResult All()
+        private const int CitiesPerPage = 15;
+
+        public CitiesController(ILekariteData data)
+            :base(data)
+        { }
+
+        public ActionResult All(int page = 1)
         {
-            return View();
+            var cities = this.Data
+                .Cities
+                .All()
+                .Project().To<CitiesViewModel>()
+                .ToList();
+
+            if (cities == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            var model = new PagedList<CitiesViewModel>(cities, page, CitiesPerPage);
+
+            return this.View(model);
         }
     }
 }
