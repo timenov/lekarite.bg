585c435fd121d21dfae2e450605132fcc2388403
Nikolay Dimitrov <niko_dimitroff@yahoo.com>
2014-11-18 08:19:29 +0200

    Add Search Controller

---
+++ Lekarite/Lekarite.Mvc/Controllers/SearchController.cs
@@ -0,0 +1,42 @@
+﻿namespace Lekarite.Mvc.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
+    using Lekarite.Data;
+    using Lekarite.Data.Interfaces;
+    using Lekarite.Models;
+    using Lekarite.Mvc.Models;
+    using Lekarite.Mvc.Models.Search;
+
+    public class SearchController : BaseController
+    {
+        private ILekariteData data;
+
+        public SearchController(ILekariteData data)
+            : base(data)
+        { }
+
+        public ActionResult Results(string search)
+        {
+            ViewData["searchString"] = search;
+            search = search.Replace(' ', '|');
+            Regex regex = new Regex(search, RegexOptions.IgnoreCase);
+
+            var results = this.Data
+                .Doctors
+                .All()
+                .Project().To<DoctorSearchViewModel>()
+                .AsEnumerable()
+                .Where(d => regex.IsMatch(d.FirstName.ToString()) | regex.IsMatch(d.LastName.ToString()));
+
+            return this.View(results);
+        }
+    }
+}
---
+++ Lekarite/Lekarite.Mvc/Models/Search/DoctorSearchViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Lekarite.Mvc.Models.Search
+{
+    using Lekarite.Models;
+    using Lekarite.Mvc.Infrastructure.Mapping;
+    
+    public class DoctorSearchViewModel : IMapFrom<Doctor>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Uin { get; set; }
+
+        public string CityName { get; set; }
+
+        public void CreateMappings(AutoMapper.IConfiguration configuration)
+        {
+            configuration.CreateMap<Doctor, DoctorSearchViewModel>()
+                .ForMember(d => d.FullName, opt => opt.MapFrom(d => d.FirstName + " " + d.LastName));
+
+            configuration.CreateMap<Doctor, DoctorSearchViewModel>()
+                .ForMember(d => d.CityName, opt => opt.MapFrom(d => d.City.Name));
+        }
+    }
+}
